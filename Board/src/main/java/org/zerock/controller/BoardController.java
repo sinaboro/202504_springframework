33cb7a5bed308f97d929ae76d5c8d8baf15ed7c4
sinaboro <rlaeocjf@gmail.com>
2025-04-28 16:38:09 +0900

    spring..

--- Board/src/main/java/org/zerock/controller/BoardController.java
+++ Board/src/main/java/org/zerock/controller/BoardController.java
@@ -10,9 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.dto.BoardVO;
 import org.zerock.service.BoardService;
 
+import lombok.RequiredArgsConstructor;
+
+/*
+/board/boardList -> 전체데이타반환
+/board/view -> 상세 페이지 
+ */
+
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
-
 	
+	private final BoardService boardService;
+	
+	@GetMapping("/boardList")
+	public String boardList(Model model) {
+		
+		List<BoardVO> list = boardService.selectListBoard();
+		
+		model.addAttribute("boardList", list);
+		
+		return "boardList";
+	}
 }
