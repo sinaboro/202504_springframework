253c4b7f1a4b2669d60d8823bd3502f3c6fe1fd2
sinaboro <rlaeocjf@gmail.com>
2025-05-13 13:05:04 +0900

    spring..

--- ex03/src/main/java/org/zerock/controller/ReplyController.java
+++ ex03/src/main/java/org/zerock/controller/ReplyController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Criterial;
 import org.zerock.domain.ReplyVO;
@@ -55,6 +57,34 @@ public class ReplyController {
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
 	}
 	
+	@GetMapping(value = "/{rno}",
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno){
+		log.info("get..........." + rno);
+		
+		return new ResponseEntity<ReplyVO>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+		log.info("remove : "+ rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<String>("seccess", HttpStatus.OK)
+										: new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);	
+	}
+	
+	@RequestMapping(method   = {RequestMethod.PUT, RequestMethod.PATCH},
+					value = "/{rno}",
+					consumes = "application/json",
+					produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno){
+		
+		vo.setRno(rno);
+		log.info("modify : " + rno);
+		
+		return service.modify(vo) == 1 ? new ResponseEntity<String>("seccess", HttpStatus.OK)
+				: new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);	
+	}
 	
 }
 
