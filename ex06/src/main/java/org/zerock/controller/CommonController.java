eaec84205412bc21c233d27c67387f3df38cef93
sinaboro <rlaeocjf@gmail.com>
2025-05-28 16:00:57 +0900

    spring..

--- ex06/src/main/java/org/zerock/controller/CommonController.java
+++ ex06/src/main/java/org/zerock/controller/CommonController.java
@@ -16,4 +16,19 @@ public class CommonController {
 		log.info("access Denied : " + authentication );
 		model.addAttribute("msg", "접근이 거부되었습니다.");
 	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		log.info("error : " + error);
+		log.info("logout : " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "Logout!!!");
+		}
+	}
 }
